ec903b03a365a283493d5d68388cdc360ea03f34
heishanmao <zx366268928@gmail.com>
2018-01-11 17:16:17 +0800

    ReadMe update screenshot

--- GetImgTool.py
+++ GetImgTool.py
@@ -17,10 +17,9 @@ pull_screenshot()
 img = Image.open("./screenshot.png")
 
 # 用 matplot 查看测试分辨率，切割问题和选项区域
-#region = img.crop((75, 315, 1167, 789)) # iPhone 7P
 
-question  = img.crop((50, 350, 1000, 560)) # 坚果 pro1
-choices = img.crop((75, 535, 990, 1130))
+question  = img.crop((100, 360, 1000, 540)) # mi5
+choices = img.crop((140, 660, 900, 1200))
 
 plt.subplot(221)
 im = plt.imshow(img, animated=True)
